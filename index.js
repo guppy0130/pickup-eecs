af8290fe8ba8e3ea29ab62fd98286045bf728495
guppy0130 <guppy0130@yahoo.com>
2019-07-10 09:23:38 -0700

    recaptcha verification + sendgrid emailing

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const sass = require('node-sass-middleware');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const prod = process.env.PRODUCTION || true;
+
+const prod = process.env.PRODUCTION === undefined ? true : !process.env.PRODUCTION;
+
+if (!prod) {
+    require('dotenv').config();
+}
+
+const sendGrid = require('@sendgrid/mail');
+sendGrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+const rp = require('request-promise-native');
 
 const fs = require('fs');
 const filePath = 'data.txt';
@@ -13,24 +23,36 @@ const fsWrite = fs.createWriteStream(filePath, {
     flags: 'a'
 });
 
-const parser = bodyParser.urlencoded({
+const addParser = bodyParser.urlencoded({
     extended: false,
     parameterLimit: 2 // msg, tags
 });
 
+const sendParser = bodyParser.urlencoded({
+    extended: true,
+    parameterLimit: 4 // to, from, captcha, number
+});
+
 const readline = require('readline');
+let lineNo = 0;
 readline.createInterface({
     input: fs.createReadStream('data.txt')
 }).on('line', line => {
     let info = line.split('\t');
     let tags = JSON.parse(info[1]);
+    data.allLines.push(info[0]);
 
     for (let tag of tags) {
         if (data.lines[tag] === undefined) {
             data.lines[tag] = [];
         }
-        data.lines[tag].push(info[0]);
+        data.lines[tag].push({
+            number: lineNo,
+            line: info[0]
+        });
     }
+
+    lineNo++;
 });
 
 app.engine('.hbs', hbs({
@@ -50,7 +72,8 @@ app.use('/styles', express.static(`${__dirname}/styles`));
 const data = {
     title: 'Pickup-EECS',
     prod: prod,
-    lines: {}
+    lines: {},
+    allLines: []
 };
 
 const random = (max) => {
@@ -105,18 +128,20 @@ app.get('/about', (req, res) => {
 
 app.get('/:tags?', (req, res) => {
     let msg = selectRandomLine(req.params.tags ? req.params.tags.split(',') : []);
-    if (typeof msg !== 'string') {
+
+    if (!Object.keys(msg).includes('line')) {
         return res.render('404', {
             message: 'no line exists with all those tags'
         });
     }
     return res.render('index', {
         title: data.title,
-        message: msg
+        message: msg.line,
+        number: msg.number
     });
 });
 
-app.post('/add', parser, (req, res) => {
+app.post('/add', addParser, (req, res) => {
     // 409 all duplicates. This appears to be an big-O(n^2) operation
     let tags = req.body.tags.split(',');
 
@@ -151,6 +176,50 @@ app.post('/add', parser, (req, res) => {
     fsWrite.write(`\n${req.body.msg}\t${JSON.stringify(tags)}`);
     return res.status(201).render('404', {
         message: 'Thanks for submitting'
+    });
+});
+
+app.post('/send', sendParser, (req, res) => {
+    console.log(req.body);
+
+    if (Object.values(req.body).includes('')) {
+        return res.render('404', {
+            message: 'missing info'
+        });
+    }
+
+    return rp.post({
+        url: 'https://www.google.com/recaptcha/api/siteverify',
+        form: {
+            secret: process.env.RECAPTCHA_KEY,
+            response: req.body['g-recaptcha-response']
+        }
+    }).then(captchaStatus => {
+        captchaStatus = JSON.parse(captchaStatus);
+        if (!captchaStatus.success) {
+            if (captchaStatus['error-codes'].includes('timeout-or-duplicate')) {
+                throw new Error('CAPTCHA expired. Return home?');
+            } else {
+                throw new Error(captchaStatus['status-codes']);
+            }
+        }
+    }).then(() => {
+        return sendGrid.send({
+            to: req.body.to,
+            from: req.body.from,
+            subject: 'Pickup-EECS',
+            text: data.allLines[req.body.number]
+        }).then(() => {
+            return res.status(200).render('404', {
+                message: 'Email sent!'
+            });
+        }).catch(() => {
+            throw new Error('failed to send email.');
+        });
+    }).catch(err => {
+        return res.status(400).render('404', {
+            message: err
+        });
     });
 });
 
